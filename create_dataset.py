7b216ac95a37de432fe1f2152c04b822b91fa632
nguyen-tho <nguyencongtho116@gmail.com>
2023-11-06 14:45:56 +0700

    update version 2

--- create_dataset.py
+++ create_dataset.py
@@ -1,38 +1,42 @@
 import cv2
 import os
-
+from deepface import DeepFace
 
 def start_capture(name):
+        model = DeepFace.build_model("VGG-Face")
         path = "./data/" + name
-        num_of_images = 0
-        detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
         try:
             os.makedirs(path)
         except:
             print('Directory Already Created')
-        vid = cv2.VideoCapture(0)
+# Create a video capture object for your camera (usually 0 for built-in cameras)
+        cap = cv2.VideoCapture(0)
+        num_of_images = 0
         while True:
+    # Capture a frame from the camera
+            ret, frame = cap.read()
 
-            ret, img = vid.read()
-            #image = adjust_brightness(img,0.5, -30)
-            new_img = None
-            grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            face = detector.detectMultiScale(image=grayimg, scaleFactor=1.1, minNeighbors=5)
-            for x, y, w, h in face:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
-                cv2.putText(img, "Face Detected", (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
-                cv2.putText(img, str(str(num_of_images)+" images captured"), (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
-                new_img = img[y:y+h, x:x+w]
-            cv2.imshow("Face Detection", img)
-            key = cv2.waitKey(1) & 0xFF
+     #Perform face detection on the frame
+            detected_faces = DeepFace.extract_faces(frame, detector_backend='ssd',
+                                        enforce_detection=False)
 
-
+            region = detected_faces[0]["facial_area"]
+    #print(region)
+            x = region['x']
+            y = region['y']
+            w = region['w']
+            h = region['h']
+            print(x, y, w, h)
+            cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 255, 0), 2)
+            cv2.imshow("Real-time Face Detection", frame)
+            new_img = frame[y:y+h, x:x+w]
             try :
                 cv2.imwrite(str(path+"/"+name+"_"+str(num_of_images)+".jpg"), new_img)
                 num_of_images += 1
             except :
 
                 pass
+            key = cv2.waitKey(1) & 0xFF
             if key == ord("q") or key == 27 or num_of_images > 350: #take 300 frames
                 break
         cv2.destroyAllWindows()
@@ -54,17 +58,17 @@ def take_video(name, video):
     while True:
 
         ret, img = vid.read()
-        img = adjust_brightness(img, 0.5, 40)
+        #img = adjust_brightness(img, 0.5, 40)
         new_img = None
-        grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face = detector.detectMultiScale(image=grayimg, scaleFactor=1.1, minNeighbors=5)
+        #grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face = detector.detectMultiScale(image=img, scaleFactor=1.1, minNeighbors=5)
         if not ret:
             break  # Break the loop if no more frames are available
         for x, y, w, h in face:
             
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
-            cv2.putText(img, "Face Detected", (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
-            cv2.putText(img, str(str(num_of_images)+" images captured"), (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
+            #cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
+            #cv2.putText(img, "Face Detected", (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
+            #cv2.putText(img, str(str(num_of_images)+" images captured"), (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
             new_img = img[y:y+h, x:x+w]
         cv2.imshow("Face Detection", img)
         key = cv2.waitKey(1) & 0xFF
@@ -74,7 +78,7 @@ def take_video(name, video):
         except :
 
             pass
-        if key == ord("q") or key == 27 or num_of_images > 350: #take 300 frames
+        if key == ord("q") or key == 27 or num_of_images > 500: #take 500 frames
             break
         cv2.destroyAllWindows()
         return num_of_images
@@ -108,12 +112,17 @@ def read_nameslist():
     return elements
 
 def number_of_samples(name):
-    data_path = 'data'+name
+    data_path = 'data/'+name
     if os.path.exists(data_path):
-    # Get the list of files in the folder
-        files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f))]
+    # List all files in the directory
+        all_files = os.listdir(data_path)
     
-    # Count the number of files
-    num_files = len(files)  
-    return num_files 
+    # Use a list comprehension to filter for .jpg files
+        jpg_files = [file for file in all_files if file.endswith('.jpg')]
     
+    # Count the number of .jpg files
+    num_jpg_files = len(jpg_files)
+    return num_jpg_files 
+
+#print('Num of samples: '+ str(number_of_samples('tho')))
+#start_capture('tho')
--- verify_user.py
+++ verify_user.py
@@ -5,38 +5,36 @@ import time
 import csv
 import create_dataset as data
 def capture_image():
-    cascPath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(cascPath)
-
-    cap = cv2.VideoCapture(0)
-    file_name = 'captured_image.jpg'
-
     start_time = time.time()
+    file_name = 'captured_image.jpg'
+    model = DeepFace.build_model("VGG-Face")
+# Create a video capture object for your camera (usually 0 for built-in cameras)
+    cap = cv2.VideoCapture(0)
+        
     while True:
-    # Capture frame-by-frame
+    # Capture a frame from the camera
         ret, frame = cap.read()
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+     #Perform face detection on the frame
+        detected_faces = DeepFace.extract_faces(frame, detector_backend='ssd',
+                                        enforce_detection=False)
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-    # Draw a rectangle around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+        region = detected_faces[0]["facial_area"]
+    #print(region)
+        x = region['x']
+        y = region['y']
+        w = region['w']
+        h = region['h']
+            
+        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 255, 0), 2)
+        new_img = frame[y:y+h, x:x+w]
+        cv2.imshow("Real-time Face Detection", frame)
+        
     # Display the resulting frame
-        cv2.imshow('Video', frame)
         elapsed_time = time.time()-start_time
         if cv2.waitKey(1) & 0xFF == ord('q') or elapsed_time > 5: #press 'q' or wait for 5s to capture an image
-            cv2.imwrite(file_name, frame)
+            cv2.imwrite(file_name, new_img)
             break
-
 # Release the camera
     cap.release()
     cv2.destroyAllWindows()
@@ -45,22 +43,26 @@ def capture_image():
 def check(name):
     data_path = 'data/' + name
     image = capture_image()
-    checklist =  DeepFace.find(image, data_path, enforce_detection=False)
+    code = 0
+    verified_img =  DeepFace.verify(image, data_path+f'/{name}_{code}.jpg', enforce_detection=False)
     img = cv2.imread(image)
     cv2.imshow("Result", img)
+    verified = verified_img["verified"]
     #set threshold >0.5 to check the result
-    threshold = len(checklist)/data.number_of_samples(name)*1.0
-    
-    if threshold > 0.5:
+    #threshold = len(checklist)/data.number_of_samples(name)
+
+    if verified:
         info = save_log(name)
         messagebox.showinfo("Congrat", info[0]+ " has checked in at "+info[1] )
        
     else:
-       messagebox.showerror("Error", "Is not "+ name+" . Please try again")
+        messagebox.showinfo("Error", "You are not "+ name+" . Please try again")
+        
+    cv2.destroyAllWindows()
        
 def save_log(name):
     named_tuple = time.localtime() # get struct_time
-    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+    time_string = time.strftime("%d/%m/%Y, %H:%M:%S", named_tuple)
     
     # Create a list with student name and time
     log_data = [name, time_string]
@@ -76,4 +78,4 @@ def save_log(name):
 def main_app(name):
     check(name)
  
-#check('tho1')
+check('tho')
